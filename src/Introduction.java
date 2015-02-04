bad41693a13e019963e1210b920f4e32e152e607
anthony.yarosh <anthony.yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2015-02-04 15:28:52 -0700

    2nd commit

--- src/Introduction.java
+++ src/Introduction.java
@@ -3,19 +3,11 @@ import java.util.Scanner;
 public class Introduction
 	{
 	static String name;
-	private static int hp, maxhp, level, exp, nextLevel, attack, defense,
-			potions, gold, floor, weapon, armor;
-	private static int monhp, monmaxhp, monAttack, monDefense;
-	protected static GameState state;
 
 	public static void main(String[] args)
 		{
 		Introduction1();
-		}
-
-	public enum GameState
-		{
-		TOWN, DUNGEON, BATTLE
+		Movement.MovementSpace();
 		}
 
 	static void Introduction1()
@@ -23,41 +15,9 @@ public class Introduction
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome Stranger, Please Enter Your Name");
 		name = input.nextLine();
-		System.out.println("Welcome to Sword Art Online" + " " + name + ".");
-		name = input.nextLine();
-		hp = 10;
-		maxhp = 5;
-		level = 1;
-		nextLevel = 15;
-		exp = 1;
-		attack = 5;
-		defense = 3;
-		potions = 0;
-		gold = 110;
-		weapon = 1;
-		armor = 1;
-		printStats();
-		state = GameState.TOWN;
-		
-		switch (state)
-	      {
-	         case TOWN:
-	            Rooms.town(null, armor, armor);
-	            break;
-	              
-	      } 
-	   } 
+		System.out
+				.println("Welcome to the world of Hyrule, you have just woken up from a deep sleep."
+						+ " " + name + ".");
 
-		
-
-	static void printStats()
-		{
-
-		String format = "Name: %s  \n" + "HP:%9s  ATT: %7s  DEF: %7s \n"
-				+ "LVL: %7s  EXP: %7s  NXT: %7s\n" + "POTION: %4s  GOLD:%7s\n";
-		System.out.printf(format, name, hp + "/" + maxhp, attack, defense,
-				level, exp, nextLevel, potions, gold);
 		}
-
 	}
-
--- src/Rooms.java
+++ src/Rooms.java
@@ -3,133 +3,146 @@ import java.util.Scanner;
 
 public class Rooms extends Introduction
 	{
-		public static void town(Room[][] room, final int WIDTH, final int HEIGHT)
-			{
-
-			for (int i = 0; i < WIDTH; i++)
-				{
-				for (int j = 0; j < HEIGHT; j++)
-					{
-					room[i][j] = new Room(i, "", "", null);
-					}
-				}
-
-			room[0][0].setNumber(1);
-			room[0][0].setName("Town Square");
-			room[0][0].setDescription("You are warped to Town Square on Floor 1. Please input up,down,left,right to move and get to aquire items as you see them.");
-
-			room[0][1].setNumber(2);
-			room[0][1].setName("Range");
-			room[0][1].setDescription("You move to the newbie training arena.");
-			room[0][1].setItems("sword");
-			room[0][1].setItems("shield");
-
-			room[1][0].setNumber(3);
-			room[1][0].setName("Shops");
-			room[1][0].setDescription("You are browsing town wares");
-			room[1][0].setItems("potions");
-
-			room[1][1].setNumber(4);
-			room[1][1].setName("Random Encounter");
-			room[1][1].setDescription("You meet a a guide NPC. He is the guide NPC for new players.");
-			room[1][1].setItems("Newbie Guide book");
-
-			room[2][1].setNumber(5);
-			room[2][1].setName("Field");
-			room[2][1].setDescription("You are at the outskirts of town.");
-			
-			room[1][2].setNumber(6);
-			room[1][2].setName("Outer Dungeon");
-			room[1][2].setDescription("You have found a dungeon.");
-			
-			room[0][2].setNumber(7);
-			room[0][2].setName("Bridge");
-			room[0][2].setDescription("You are on the bridge.");
-			room[0][2].setItems("Bottle of Water");
-			
-			room[2][2].setNumber(8);
-			room[2][2].setName("Dungeon Enterance");
-			room[2][2].setDescription("You have entered the dungeon.");
-			
-			room[2][0].setNumber(9);
-			room[2][0].setName("View");
-			room[2][0].setDescription("You see a beautiful view.");
-			}
-		
-
-		public static void print(Room[][] room, int x, int y)
-			{
-
-			System.out.println(room[x][y].getDescription());
-			System.out.println("You see: " + room[x][y].getItems());
-			System.out.println();
-			}
-
-		public static void removeItem(Room[][] room, int x, int y, String item)
-			{
-
-			room[x][y].deleteItem(item);
-			}
-		}
-
-	class Room
+	public static void build(Room[][][] room, final int WIDTH,
+			final int HEIGHT, final int LENGTH)
 		{
 
-		private int number;
-		private String name;
-		private String description;
-		public ArrayList<String> items = new ArrayList<>();
-
-		public Room(int number, String name, String description,
-				ArrayList<String> items)
+		for (int i = 0; i < WIDTH; i++)
 			{
-			}
+			for (int j = 0; j < HEIGHT; j++)
+				{
+				for (int k = 0; k < LENGTH; k++)
+					{
+					room[i][j][k] = new Room(i, "", "", null);
+					}
 
-		public void setNumber(int number)
-			{
-			this.number = number;
-			}
+				room[0][0][0].setNumber(1);
+				room[0][0][0].setName("Town Square");
+				room[0][0][0]
+						.setDescription("You are warped to Town Square on Floor 1. Please input up,down,left,right to move and get to aquire items as you see them.");
 
-		public int getNumber()
-			{
-			return this.number;
-			}
+				room[0][1][0].setNumber(2);
+				room[0][1][0].setName("Range");
+				room[0][1][0]
+						.setDescription("You move to the newbie training arena.");
+				room[0][1][0].setItems("sword");
+				room[0][1][0].setItems("shield");
 
-		public void setName(String name)
-			{
-			this.name = name;
-			}
+				room[1][0][0].setNumber(3);
+				room[1][0][0].setName("Shops");
+				room[1][0][0].setDescription("You are browsing town wares");
+				room[1][0][0].setItems("potions");
 
-		public String getName()
-			{
-			return this.name;
-			}
+				room[1][1][0].setNumber(4);
+				room[1][1][0].setName("Random Encounter");
+				room[1][1][0]
+						.setDescription("You meet a a guide NPC. He is the guide NPC for new players.");
+				room[1][1][0].setItems("Newbie Guide book");
 
-		public void setDescription(String description)
-			{
-			this.description = description;
-			}
+				room[2][1][0].setNumber(5);
+				room[2][1][0].setName("Field");
+				room[2][1][0]
+						.setDescription("You are at the outskirts of town.");
+				room[2][1][0].setItems("sword");
 
-		public String getDescription()
-			{
-			return this.description;
-			}
+				room[1][2][0].setNumber(6);
+				room[1][2][0].setName("Outer Dungeon");
+				room[1][2][0]
+						.setDescription("You have found a dungeon. Would you like to enter?");
 
-		public void setItems(String item)
-			{
-			this.items.add(item);
-			}
+				room[0][2][0].setNumber(7);
+				room[0][2][0].setName("LonLonRanch");
+				room[0][2][0].setDescription("You have found Lon Lon Ranch.");
+				room[0][2][0].setItems("Lon Lon Milk");
 
-		public void deleteItem(String item)
-			{
-			this.items.remove(item);
-			}
+				room[2][2][0].setNumber(8);
+				room[2][2][0].setName("Armory");
+				room[2][2][0]
+						.setDescription("You have entered the hylian armory.");
+				room[0][2][0].setItems("Lon Lon Milk");
 
-		public ArrayList<String> getItems()
-			{
-			return this.items;
+				room[2][0][0].setNumber(9);
+				room[2][0][0].setName("Viewpoint");
+				room[2][0][0]
+						.setDescription("You see a beautiful view of Hyrule Fields.");
+				}
 			}
-		
-
 		}
-	
+
+	public static void print(Room[][][] room, int x, int y, int z)
+		{
+		System.out.println(room[x][y][z].getDescription());
+		if (!room[x][y][z].getItems().equals(""))
+			{
+			System.out.println("You see: " + room[x][y][z].getItems());
+			System.out.println();
+			}
+		}
+
+	public static void removeItem(Room[][][] room, int x, int y, int z,
+			String item)
+		{
+
+		room[x][y][z].deleteItem(item);
+		}
+	}
+
+class Room
+	{
+
+	private int number;
+	private String name;
+	private String description;
+	public ArrayList<String> items = new ArrayList<>();
+
+	public Room(int number, String name, String description,
+			ArrayList<String> items)
+		{
+		}
+
+	public void setNumber(int number)
+		{
+		this.number = number;
+		}
+
+	public int getNumber()
+		{
+		return this.number;
+		}
+
+	public void setName(String name)
+		{
+		this.name = name;
+		}
+
+	public String getName()
+		{
+		return this.name;
+		}
+
+	public void setDescription(String description)
+		{
+		this.description = description;
+		}
+
+	public String getDescription()
+		{
+		return this.description;
+		}
+
+	public void setItems(String item)
+		{
+		this.items.add(item);
+		}
+
+	public void deleteItem(String item)
+		{
+		this.items.remove(item);
+		}
+
+	public ArrayList<String> getItems()
+		{
+		return this.items;
+		}
+
+	}
